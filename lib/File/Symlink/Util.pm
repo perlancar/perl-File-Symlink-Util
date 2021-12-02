01cfe54242ae6d3bc35ad0b02632d461f480e415
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-12-02 08:15:44 +0700

    Initial commit

---
+++ lib/File/Symlink/Util.pm
@@ -0,0 +1,40 @@
+package File::Symlink::Util;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Exporter 'import';
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our @EXPORT_OK = qw(
+               );
+
+1;
+# ABSTRACT: Symlink-related utilities
+
+=head1 SYNOPSIS
+
+ use File::Symlink::Util qw(
+ );
+
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS
+
+
+=head1 SEE ALSO
+
+=head2 Other symlink-related routines
+
+L<File::Symlink::Relative> provides C<symlink_r> to create relative symlinks.
+
+L<File::MoreUtil> provides C<file_exists> and C<l_abs_path>.
+
+=cut
